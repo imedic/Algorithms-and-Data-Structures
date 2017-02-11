9a09b237b284545488926e735bb7ef3c56e75144
imedic <medic.ivan.hr@gmail.com>
2017-02-12 00:03:52 +0100

    Exercise 7 added

---
+++ 7 - Hash Tables/Hash Tables/Hash Tables/HashTable.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hash_Tables
+{
+    class HashTable
+    {
+        Node[] buckets;
+        int length;
+
+        public HashTable(int length)
+        {
+            this.length = length;
+            buckets = new Node[length];
+        }
+
+        public void Display()
+        {
+            for (int bucket = 0; bucket < buckets.Length; bucket++)
+            {
+                Node current = buckets[bucket];
+                Console.Write(bucket + ": ");
+                while (current != null)
+                {
+                    Console.Write("[" + current.Name + "," + current.Value + "] ");
+                    current = current.Next;
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private int Hash(string str)
+        {
+            int h = 0;
+
+            foreach (var s in str)
+                h = 37 * h + s;
+
+            return h % length;
+        }
+
+        public void Insert(string name, int value)
+        {
+            int bucket = Hash(name);
+            buckets[bucket] =
+            new Node(name, value, buckets[bucket]);
+        }
+
+        public int Search(string name)
+        {
+            Node current = buckets[Hash(name)];
+            while (current != null)
+            {
+                if (current.Name.Equals(name))
+                    return current.Value;
+
+                current = current.Next;
+            }
+            throw new Exception(name + " Not found.");
+        }
+
+        public void Delete(string name)
+        {
+            int bucket = Hash(name);
+            Node current = buckets[bucket];
+            Node previous = null;
+
+            while (current != null)
+            {
+                if (current.Name.Equals(name))
+                {
+                    if (previous == null)
+                    {
+                        buckets[bucket] = current.Next;
+                    }
+                    else
+                    {
+                        previous.Next = current.Next;
+                    }
+                    return;
+                }
+                previous = current;
+                current = current.Next;
+            }
+            throw new Exception(name + " Not found.");
+        }
+    }
+}
---
+++ 7 - Hash Tables/Hash Tables/Hash Tables/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hash_Tables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashTable table = new HashTable(9);
+            table.Insert("Ivo", 5);
+            table.Insert("Ana", 1);
+            table.Insert("Joe", 7);
+            table.Insert("Bob", 4);
+            table.Insert("Kim", 3);
+            table.Insert("Ena", 8);
+            table.Insert("Iva", 2);
+            table.Insert("Tea", 9);
+            table.Insert("Lea", 6);
+
+            table.Display();
+
+            string name = "Bob";
+
+            try
+            {
+                int value = table.Search(name);
+                Console.WriteLine(name + ", " + value);
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine(x.Message);
+            }
+            try
+            {
+                table.Delete(name);
+                table.Delete("Joe");
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine(x.Message);
+            }
+
+            table.Display();
+        }
+    }
+}
